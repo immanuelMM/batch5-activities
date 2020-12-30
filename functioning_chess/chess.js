91633e03d1507830ede78c65e826fdc6209c19be
immanuelMM <merino.mahusay@gmail.com>
2020-12-30 20:14:41 +0800

    add timer not functioning

--- functioning_chess/chess.js
+++ functioning_chess/chess.js
@@ -70,6 +70,7 @@ function createPiece(cell) {
 	img = document.createElement('img')
 	img.setAttribute("data-piece", piece)
 	img.classList.add('piece')
+
 	if (i == 0 || i == 1) {
 		img.src = `white_images/${piece}.png`
 		img.setAttribute("data-color", "white")
