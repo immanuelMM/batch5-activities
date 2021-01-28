f48fe87533523e250609cadd613c0995b26b9364
immanuelMM <merino.mahusay@gmail.com>
2021-01-28 21:21:05 +0800

    fix the bug for timer

--- functioning_chess/chess.js
+++ functioning_chess/chess.js
@@ -3,6 +3,24 @@
 /// add a global event listener for clicks
 /// have global variables for element_selected and turn(which is the color)
 
+// white div
+const white = document.getElementById('white1');
+// black div
+const black = document.getElementById('black1');
+//control row elements
+const startTime = document.getElementById('times');
+/* const minBtn = document.getElementById('minBtn'); */
+const reset = document.querySelector('.reset-btn');
+  // randoms for functions
+var sec;
+var min;
+var nWhtIntId;
+var nBlkIntId;
+const whtSideColor = white.style.background;
+const blkSideColor = black.style.background;
+
+
+
 
 let turn_color = "white"
 let turns = []
@@ -195,11 +213,11 @@ function nextMove(e) {
 		from_coords = to_coords
 		from_cell = to_cell
 		from_cell.classList.add('selected')
+	
 	}
 	else if (from_coords.toString() === to_coords.toString()){
 
 		removeSelection()
-
 	}
 	else if (from_coords.length === 2 && ((to_cell.firstChild && to_cell.firstChild.dataset.color !== turn_color)
 		|| !to_cell.firstChild)) {
@@ -221,6 +239,13 @@ function nextMove(e) {
 function removeSelection() {
 	if (from_cell.classList) {
 		from_cell.classList.remove('selected')
+		
+		if (turn_color == "white"){
+			whtMaster();
+		}else{
+			blkMaster();
+		}
+
 	}
 	from_cell = ''
 	from_coords = []
@@ -434,4 +459,135 @@ function interfaceGrid(id) {
 	horizontal = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 	return `${horizontal[x]}, ${+y + 1}`
 }
+
+
+// FUNCTIONS
+  // picking time control
+const setTime = function (event) {
+  document.getElementById('blkmin').innerHTML = document.getElementById('times').value;
+  document.getElementById('whtmin').innerHTML = document.getElementById('times').value;
+};
+  // reset button
+const resetClick = function (event) {
+  whtStop();
+  blkStop();
+  document.getElementById('blkmin').innerHTML = document.getElementById('times').value;
+  document.getElementById('whtmin').innerHTML = document.getElementById('times').value;
+  document.getElementById('blksec').innerHTML = "00";
+  document.getElementById('whtsec').innerHTML = "00";
+  white.style.background = whtSideColor;
+  black.style.background = blkSideColor;
+  show();
+  black.style.pointerEvents = "auto";
+  white.style.pointerEvents = "none";
+  times.style.pointerEvents = "auto";
+};
+  // alert: work in progress
+const learning = function () {
+  alert('Learning how to implement spacebar/keyboard control...check back in!');
+};
+  // black second countdown
+const blkTicker = function() {
+  sec = document.getElementById('blksec').innerHTML;
+  min = document.getElementById('blkmin').innerHTML;
+  if (sec >= 11){
+    sec--;
+  document.getElementById('blksec').textContent = sec;
+  }
+  else if (sec >= 1) {
+    sec--;
+  document.getElementById('blksec').textContent = '0' + sec;
+  }
+  else if (sec == 0 && min >= 1){
+    sec = 59;
+    min--;
+  document.getElementById('blksec').textContent = sec;
+  document.getElementById('blkmin').textContent = min;
+  }
+ else {
+   black.style.background = 'red';
+ }
+};
+ // white second countdown
+const whtTicker = function() {
+  sec = document.getElementById('whtsec').innerHTML;
+  min = document.getElementById('whtmin').innerHTML;
+  if (sec >= 11){
+    sec--;
+  document.getElementById('whtsec').textContent = sec;
+  }
+  else if (sec >= 1) {
+    sec--;    document.getElementById('whtsec').textContent = '0' + sec;
+  }
+  else if (sec == 0 && min >= 1){
+    sec = 59;
+    min--;
+  document.getElementById('whtsec').textContent = sec;
+  document.getElementById('whtmin').textContent = min;
+
+  }
+
+  else {
+  white.style.background = "red";
+ };
+};
+  // interval start black countdown
+const blkCountdown = function () {
+  nBlkIntId = setInterval(blkTicker, 1000);
+};
+// interval start white countdown
+const whtCountdown = function () {
+  nWhtIntId = setInterval(whtTicker, 1000);
+};
+  // end black countdown
+const blkStop = function () {
+  clearInterval(nBlkIntId);
+};
+  // end white countdown
+const whtStop = function () { clearInterval(nWhtIntId);
+};
+  // hide instructions at game start
+const hide = function () {
+  document.getElementById('startInstructions').style.display = "none";
+};
+  // show instructions at game reset
+const show = function () {
+  document.getElementById('startInstructions').style.display = "block";
+};
+  // hide mobile instructions
+const hideMobile = function () {  document.getElementById('mobileIns').style.display = "none";
+};
+  // show mobile instructions
+const showMobile = function () {
+  document.getElementById('mobileIns').style.display = 'block';
+};
+  // black's function container function
+const blkMaster = function () {
+  whtCountdown();
+  blkStop();
+  hide();
+  hideMobile();
+  black.style.pointerEvents = "none";
+  white.style.pointerEvents = "auto";
+  times.style.pointerEvents = "none";
+};
+  // white's function container function
+const whtMaster = function () {
+   blkCountdown();
+  whtStop();
+  white.style.pointerEvents = "none";
+  black.style.pointerEvents = "auto";
+};
+
+//EVENTS
+  // white side 
+white.addEventListener('click', whtMaster);
+  // black side
+black.addEventListener('click', blkMaster);
+  // picking time control
+startTime.addEventListener('change', setTime);
+  // alert: work in progress
+window.addEventListener('keypress', learning);
+  // reset 
+reset.addEventListener('click', resetClick);
 
