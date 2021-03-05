fbe5f887b1d10436e625e38f1ce83ffbfbec05bc
immanuelMM <merino.mahusay@gmail.com>
2021-03-05 18:58:39 +0800

    determin if perfect sqrt

---
+++ Back-End/Ruby/activity/square.rb
@@ -0,0 +1,30 @@
+def square_root_me(num)
+   start = num.to_s(2); start = start[0,(start.length/2)].to_i(2)
+   retval = start
+   for i in 0..10000 do
+     retval = ((retval + num/retval)/2).to_i
+     return retval 
+     if retval*retval == num 
+     end
+   end
+ end
+def is_square?(x)
+
+  if x == 0 
+    puts "#{x} - #{true}"
+    return true
+  end
+    sqrt = square_root_me(x)
+    result = x  == sqrt * sqrt
+    puts "#{x} - #{result}"
+    return result
+  end
+
+  is_square?(0)
+  is_square?(3)
+  is_square?(4)
+  is_square?(25)
+  is_square?(26)
+
+
+
